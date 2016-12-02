a637b65e0fbb7ab2553712dda889e6d213b58ac1
Andrei <andreydamian2620000@gmail.com>
2016-12-02 16:44:16 +0200

    Region

---
+++ src/region.cxx
@@ -0,0 +1,6 @@
+#include "region.hpp"
+
+namespace redi
+{
+
+} // namespace redi
---
+++ src/region.hpp
@@ -0,0 +1,22 @@
+#ifndef REDI_REGION
+#define REDI_REGION
+
+#include <fstream>
+
+namespace redi
+{
+
+class Region
+{
+public:
+  
+private:
+  
+  std::fstream mFile;
+  
+};
+
+} // namespace redi
+
+
+#endif // REDI_REGION
