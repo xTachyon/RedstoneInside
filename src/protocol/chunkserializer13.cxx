528fa1bfc30a8aa312b6386a88c1e0fc74f9fbf1
Damian Andrei <xTachyon@users.noreply.github.com>
2017-01-20 12:03:19 +0200

    Chunk Data working with hardcoded values

--- src/protocol/chunkserializer13.cxx
+++ src/protocol/chunkserializer13.cxx
@@ -9,15 +9,82 @@ ChunkSerializer13::ChunkSerializer13(const Chunk& chunk, Vector2i pos, Dimension
 
 ByteBuffer ChunkSerializer13::operator()()
 {
-  PacketWriter writer(0x20); // packet id
-  
-  writeHeader(writer);
-  writeChunkSections(writer);
-  writeBiomes(writer);
-  writeBlockEntities(writer);
-  
-  writer.commit();
-  return writer;
+  PacketWriter packet(0x20); // packet id
+
+//  writeHeader(writer);
+//  writeChunkSections(writer);
+//  writeBiomes(writer);
+//  writeBlockEntities(writer);
+//
+//  writer.commit();
+//  return writer;
+
+	packet.writeInt(mPosition.x);
+	packet.writeInt(mPosition.z);
+	packet.writeBool(true);        // "Ground-up continuous", or rather, "biome data present" flag
+	packet.writeVarInt(0xFFFF);  // We're aways sending the full chunk with no additional data, so the bitmap is 0xffff
+	// Write the chunk size:
+    const size_t ChunkSectionDataArraySize = (BlocksPerSection * BitsPerBlock) / 8 / 8;  // Convert from bit count to long count
+	std::size_t ChunkSectionSize = (
+		1 +
+		1 +
+		2 +
+		ChunkSectionDataArraySize * 8 +
+		BlocksPerSection / 2
+	);
+
+	if (mDimension == Dimension::Overworld)
+	{
+		// Sky light is only sent in the overworld.
+		ChunkSectionSize += BlocksPerSection / 2;
+	}
+
+	const size_t BiomeDataSize = 16 * 16;
+	size_t ChunkSize = (
+		ChunkSectionSize * 16 +
+		BiomeDataSize
+	);
+	packet.writeVarInt(ChunkSize);
+
+	// Write each chunk section...
+	for (size_t index = 0; index < 16; index++)
+	{
+		packet.writeByte(BitsPerBlock);
+		packet.writeVarInt(0);  // Palette length is 0
+		packet.writeVarInt(ChunkSectionDataArraySize);
+
+		for (std::size_t i = 0; i < ChunkSectionDataArraySize; ++i)
+		{
+		    //srand(time(nullptr));
+			packet.writeLong(1);
+		}
+
+		// Light - stored as a nibble, so we need half sizes
+		// As far as I know, there isn't a method to only write a range of the array
+		writeBlockLight(packet, index);
+		//for (size_t Index = 0; Index < ChunkSectionBlocks / 2; Index++)
+		//{
+		//	packet.writeUByte(0xFF);
+		//}
+		if (mDimension == Dimension::Overworld)
+		{
+			// Skylight is only sent in the overworld; the nether and end do not use it
+			//for (size_t Index = 0; Index < ChunkSectionBlocks / 2; Index++)
+			//{
+			//	packet.writeUByte(0xFF);
+			//}
+			writeBlockLight(packet, index);
+		}
+	}
+
+	// Write the biome data
+//	Packet.WriteBuf(m_BiomeData, BiomeDataSize);
+	writeBiomes(packet);
+	// Identify 1.9.4's tile entity list as empty
+	packet.writeUByte(0);
+
+	packet.commit();
+	return packet;
 }
 
 void ChunkSerializer13::writeHeader(PacketWriter& writer)
@@ -28,14 +95,14 @@ void ChunkSerializer13::writeHeader(PacketWriter& writer)
         2 +
         ChunkSectionDataSize * 8 +
         BlocksPerSection / 2;
-  
+
   if (mDimension == Dimension::Overworld)
   {
     sectionsize += BlocksPerSection / 2;
   }
-  
+
   std::size_t chunksize = sectionsize * 16 + BiomeDataSize;
-  
+
   writer.writeInt(mPosition.x); // chunk x
   writer.writeInt(mPosition.z); // chunk z
   writer.writeBool(true); // Ground-Up Continuous
@@ -56,11 +123,11 @@ void ChunkSerializer13::writeChunkSection(PacketWriter& writer, std::uint8_t nth
   writer.writeUByte(BitsPerBlock); // Bits Per Block
   writer.writeUByte(0); // Palette length
   writer.writeVarInt(ChunkSectionDataSize); // Data Array Length
-  
-  std::uint64_t value = 0;
-  std::size_t offset = 0;
-  std::size_t shift = 0;
-  
+
+//  std::uint64_t value = 0;
+//  std::size_t offset = 0;
+//  std::size_t shift = 0;
+
 //  for (std::uint16_t x = 0; x < Chunk::ChunkMaxX; ++x)
 //  {
 //    for (std::uint16_t z = 0; z < Chunk::ChunkMaxZ; ++z)
@@ -88,12 +155,12 @@ void ChunkSerializer13::writeChunkSection(PacketWriter& writer, std::uint8_t nth
 //      }
 //    }
 //  }
-  
+
   for (std::size_t i = 0; i < ChunkSectionDataSize; ++i)
   {
     writer.writeVarLong(0);
   }
-  
+
 //  writer.writeVarLong(value);
   writeBlockLight(writer, nth); // Block Light
   if (mDimension == Dimension::Overworld) writeSkyLight(writer, nth); // Sky Light
@@ -118,7 +185,7 @@ void ChunkSerializer13::writeSkyLight(PacketWriter& writer, std::uint8_t)
 void ChunkSerializer13::writeBiomes(PacketWriter& writer)
 {
   const Chunk::ChunkColumns& c = mChunk.getChunkColumns();
-  
+
   for (std::size_t x = 0; x < 16; ++x)
   {
     for (std::size_t z = 0; z < 16; ++z)
@@ -135,9 +202,10 @@ void ChunkSerializer13::writeBlockEntities(PacketWriter& writer)
 
 std::uint64_t ChunkSerializer13::generateBlockStateID(Block b)
 {
-  constexpr std::uint64_t OverflowMask = 0b111111111;
+  constexpr std::uint64_t OverflowMask = (1 << BitsPerBlock) - 1;
+  //constexpr std::uint64_t OverflowMask = 0b111111111;
   return ((static_cast<std::uint64_t>(b.type) & OverflowMask)
         << 4) | b.data;
 }
-  
+
 } // namespace redi
--- src/protocol/chunkserializer13.hpp
+++ src/protocol/chunkserializer13.hpp
@@ -11,23 +11,26 @@ namespace redi
 class ChunkSerializer13
 {
   public:
-  
+
   ChunkSerializer13(const Chunk& chunk, Vector2i pos, Dimension dimension = Dimension::Overworld);
-  
+
   ByteBuffer operator()();
-  
+
   private:
-  
+
   static constexpr std::size_t ChunkSectionsNumber = 16;
   static constexpr std::size_t BitsPerBlock = 13;
   static constexpr std::size_t BlocksPerSection = 16 * 16 * 16;
   static constexpr std::size_t ChunkSectionDataSize = BlocksPerSection * BitsPerBlock / 8 / 8;
   static constexpr std::size_t BiomeDataSize = 256;
-  
+
+  const size_t NumChunkSections = 16;
+  const size_t ChunkSectionDataArraySize = (BlocksPerSection * BitsPerBlock) / 8 / 8;  // Convert from bit count to long count
+
   const Chunk& mChunk;
   Vector2i mPosition;
   Dimension mDimension;
-  
+
   void writeHeader(PacketWriter& writer);
   void writeChunkSections(PacketWriter& writer);
   void writeChunkSection(PacketWriter& writer, std::uint8_t nth);
@@ -35,10 +38,10 @@ class ChunkSerializer13
   void writeSkyLight(PacketWriter& writer, std::uint8_t nth);
   void writeBiomes(PacketWriter& writer);
   void writeBlockEntities(PacketWriter& writer);
-  
+
   static uint64_t generateBlockStateID(Block b);
 };
-  
+
 } // namespace redi
 
 #endif // REDI_CHUNKSERIALIZER13_HPP
