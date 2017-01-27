e3973e6c0ab0f7d0f4c93e7097a0cfdd9188bc83
Andrei <andreidaamian@gmail.com>
2017-01-27 11:01:12 +0200

    T

--- src/player.cxx
+++ src/player.cxx
@@ -10,11 +10,11 @@ Player::Player(const std::string& name, boost::uuids::uuid uuid, Session* sessio
                World* world, Gamemode gamemode)
     : mUUID(uuid), mNickname(name), mServer(server), mWorld(world), mSession(session), mGamemode(gamemode), mDimension(Dimension::Overworld), mSendKeepAlive(session->getIoService()),
       mEntityID(id)
-//  : mEntityID(id), mNickname(name), mUUID(uuid), mSession(session), mServer(server), mGamemode(gamemode), mSendKeepAlive(session->getIoService()), mDimension(Dimension::Overworld),
+//  : mEnttyID(id), mNickname(name), mUUID(uuid), mSession(session), mServer(server), mGamemode(gamemode), mSendKeepAlive(session->getIoService()), mDimension(Dimension::Overworld),
 //    mWorld(world)
 {
   Logger::info((boost::format("%1% has joined the game") % mNickname).str());
-  
+
   mSendKeepAlive.expires_from_now(boost::posix_time::seconds(5));
   mSendKeepAlive.async_wait(boost::bind(&Player::onSendKeepAliveTimerRing, boost::asio::placeholders::error, std::addressof(mSendKeepAlive), &mSession->getProtocol()));
 }
@@ -31,11 +31,11 @@ void Player::onSendKeepAliveTimerRing(const boost::system::error_code& error, bo
   if (!error)
   {
     protocol->sendKeepAlive();
-    
+
     timer->expires_from_now(boost::posix_time::seconds(10));
     timer->async_wait(boost::bind(&Player::onSendKeepAliveTimerRing, boost::asio::placeholders::error, timer, protocol));
   }
-  
+
   Logger::debug("Send Keep Alive Timer Ring " + std::to_string(error.value()));
 }
 
@@ -53,5 +53,5 @@ bool operator!=(const Player& l, const Player& r)
 {
   return !(l == r);
 }
-  
+
 } // namespace red
