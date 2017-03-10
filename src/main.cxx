ad14a9c7a6e103c15806f449b41a4a60243ebac5
Andrei <andreidaamian@gmail.com>
2017-03-10 11:10:30 +0200

    Regions won't work yet

--- src/main.cxx
+++ src/main.cxx
@@ -15,26 +15,28 @@ int main(int, char**)
   using namespace redi::nbt;
   using namespace std::string_literals;
   
-  while (true)
-  {
-    std::string path;
-    if (path == "*")
-    {
-      break;
-    }
-    
-    int x, z;
-    std::cin >> x >> z;
-    std::string p = (boost::format("/home/andrei/.minecraft/saves/redi/region/r.%1%.%2%.mca") % x % z).str();
-    
-    world::Anvil a(p, Vector2i(x, z));
-    
-    ByteBuffer buffer;
-    auto xz = a.readChunk(Vector2i(-1, -10), buffer);
-    a.writeChunk(Vector2i(-1, -10), buffer);
-    
-    std::cout;
-  }
+//  while (true)
+//  {
+//    std::string path;
+//    if (path == "*")
+//    {
+//      break;
+//    }
+//
+//    int x, z;
+//    std::cin >> x >> z;
+//    std::string p = (boost::format("/home/andrei/.minecraft/saves/redi/region/r.%1%.%2%.mca") % x % z).str();
+//
+//    world::Anvil a(p, Vector2i(x, z));
+//
+//    ByteBuffer buffer;
+//    auto xz = a.readChunk(Vector2i(-1, -10), buffer);
+//    std::ofstream("in.data").write(buffer.as_const_char(), buffer.size());
+//
+//    a.writeChunk(Vector2i(-1, -10), buffer);
+//
+//    std::cout;
+//  }
   
   redi::Logger::debug("Redi is starting");
   
