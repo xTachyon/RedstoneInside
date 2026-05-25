8f8270ea65f62644e82b2c71d66236eee7e741c3
Andrei Damian <andreidaamian@gmail.com>
2026-05-25 20:08:45 +0300

    upgrade

--- compile.sh
+++ compile.sh
@@ -3,6 +3,10 @@ set -ex
 
 cd /x
 
+
+export CC=clang
+export CXX=clang++
+
 rm -rf build/CMakeCache.txt
 # cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -G Ninja
 # cmake --build build
@@ -10,5 +14,5 @@ rm -rf build/CMakeCache.txt
 cmake --version
 mkdir -p build
 cd build
-cmake .. -DBOOST_INCLUDEDIR=/libs/boost/include -DBOOST_LIBRARYDIR=/libs/boost/lib -DJSON_REPO_DIR=/libs/json -G Ninja
+cmake .. -DBOOST_INCLUDEDIR=/libs/boost/include -DBOOST_LIBRARYDIR=/libs/boost/lib -DJSON_REPO_DIR=/libs/json -DCMAKE_BUILD_TYPE=Debug -G Ninja
 cmake --build .
--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 set -ex
 
-podman build .
-id=$(podman build . -q)
+if [ -z "id" ]; then
+    podman build .
+    id=$(podman build . -q)
+fi
 
 podman run --rm -v .:/x:z --pull never $id
